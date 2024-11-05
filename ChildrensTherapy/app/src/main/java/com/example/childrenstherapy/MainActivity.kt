006f0880144dcf1ad962f48ad4eabcb4a64784dd
StudleyBryce <bas824@msstate.edu>
2024-11-05 11:23:46 -0600

    Updated formatting for button press code

--- ChildrensTherapy/app/src/main/java/com/example/childrenstherapy/MainActivity.kt
+++ ChildrensTherapy/app/src/main/java/com/example/childrenstherapy/MainActivity.kt
@@ -1,6 +1,13 @@
+/* #### This is the code for the button and the ability to press it to get a reaction
+
+   These are the nessasary imports, described in detail below */
+
 package com.example.childrenstherapy
 
 import android.os.Bundle
+/* Bundle, imported above, allows the app to save the activity and keep your
+   state in the program. With bundle, if your app is moved to the background, it
+   will return to the state you left it. */
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -23,19 +30,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.childrenstherapy.ui.theme.ChildrensTherapyTheme
-/*
-Similar to int main() entry point in C++, but this is specific to Android app development.
-"class MainActivity" is declared in the AndroidManifest.xml file.
-Bundle, imported above, allows the app to save the activity and keep your state in the
-program. With bundle, if your app is moved to the background, it will return to the state
-you left it.  The syntax class_name : other_class_name signifies MainActivity inherits all
-methods from the other class, ComponentActivity.  Scaffold, a Jetpack Compose function,
-provides UI layouts (such as having a title on top, a button in the bottom right, and a
-bottom bar) and standard spacing.
-https://www.geeksforgeeks.org/scaffold-in-android-using-jetpack-compose/?ref=header_outind
-Placing the Scaffold function under setContent ensures it takes up the entire available
-space.
- */
+/* Similar to int main() entry point in C++, but this is specific to Android app
+   development."
+
+   Bundle, imported above, allows the app to save the activity and keep your
+   state in the program. With bundle, if your app is moved to the background, it
+   will return to the state you left it.
+
+   Class: Main
+
+   <span style="caret-color: initial;">class</span> <span style="caret-color: initial;">&nbsp;</span>Activity<span style="caret-color: initial;">MainActivity"
+   is declared in the AndroidManifest.xml file.</span>
+
+   The syntax class_name : other_class_name signifies MainActivity inherits all
+   methods from the other class, ComponentActivity.Scaffold, a Jetpack Compose
+   function, provides UI layouts (such as having a title on top, a button in the
+   bottom right, and a bottom bar) and standard spacing.
+   https://www.geeksforgeeks.org/scaffold-in-android-using-jetpack-compose/?ref=header\_outind
+   Placing the Scaffold function under setContent ensures it takes up the entire
+   available space. */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,34 +64,32 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-/*
-A composable is a "nestable" function from Jetpack that allows you to create complex
-UI structures. So "@Composable" tells the function "Greeting" to use the Jetpack
-Compose UI framework.
- */
+/* A composable is a "nestable" function from Jetpack that allows you to create
+   complex UI structures. So "@Composable" tells the function "Greeting" to use
+   the Jetpack Compose UI framework. */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var helloEmoji by remember { // var is mutable variable, vs val which is not.
         mutableStateOf(false)
     }
-    /*
-    Function "Column" arranges items vertically.  The function "Button" is placed within this column
-    that shows "Hi, my name is ...".  When this button is clicked, the state of "helloEmoji" is set to
-    true and a GIF emoji is displayed.
-     */
+    /* ```
+       Function "Column" arranges items vertically.  The function "Button" is placed within this column
+       that shows "Hi, my name is ...".  When this button is clicked, the state of "helloEmoji" is set to
+       true and a GIF emoji is displayed.
+       ``` */
     Column( // Function imported from import androidx.compose.foundation.layout.
         // Column gives Greeting a vertical layout.
         horizontalAlignment  = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.padding(24.dp) // Modifier function contains decorations and
         // behaviors to function Column.
-        // https://www.geeksforgeeks.org/concept-of-padding-in-android/?ref=header_outind
+        // https://www.geeksforgeeks.org/concept-of-padding-in-android/?ref=header\_outind
 
     )
-    /*
-    Button is a Composable function that creates a clickable button UI.
-    This function provides a string of text (Hi, my name is $name!"
-    */
+    /* ```
+       Button is a Composable function that creates a clickable button UI.
+       This function provides a string of text (Hi, my name is $name!"
+       ``` */
     {
         Button(
             onClick = { helloEmoji = true }, // Sets a boolean state to var "helloEmoji".
@@ -98,9 +109,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }
     }
 }
-/*
-@Preview allows us to preview the app inside of the Android Studio environment.
- */
+/* @Preview allows us to preview the app inside of the Android Studio
+   environment. */
 @Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
