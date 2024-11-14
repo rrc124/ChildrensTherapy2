8115ace8ad0615ce6d823ca9de09b26d2d17b4c7
dh2540 <dh2540@msstate.edu>
2024-11-14 10:22:49 -0600

    Fixing mainacitivty

--- ChildrensTherapy/app/src/main/java/com/example/childrenstherapy/MainActivity.kt
+++ ChildrensTherapy/app/src/main/java/com/example/childrenstherapy/MainActivity.kt
@@ -6,6 +6,10 @@ import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.net.Uri
+import android.view.View
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
 
 
 Syntax: class MainActivity "of" (:) AppCompatActivity()
@@ -13,6 +17,7 @@ AppCompatActivity() supports modern themes while offering compatibility to older
 This is set when you choose backwards compatibility upon setting up the project.
  */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         // When we override a function, we can use super.method to call the superclass' version of the method
@@ -31,6 +36,15 @@ class MainActivity : AppCompatActivity() {
             // Other methods in Log.<method> are: Log.v (low level info); Log.i (general info); Log.w (warnings); Log.e (error messages)
             Log.d("MainActivity", "Open Webpage button clicked")
         }
+
+        findVideoViewID<Button2>(R.id.button_open_webpage).setOnClickListener {
+            val videoUrl = "https://www.google.com"
+            VideoView(videoUrl)
+            // Log.d sends debugging information to Logcat so we can view what's going on.
+            // Other methods in Log.<method> are: Log.v (low level info); Log.i (general info); Log.w (warnings); Log.e (error messages)
+            Log.d("MainActivity", "Open Video button clicked") 
+        }
+        
     }
     // Function takes a URL to op0en a webpage.
     private fun openWebPage(url: String) {
@@ -50,5 +64,27 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.d("MainActivity", "No application can handle this intent.")
         }
+    } 
+
+    private fun VideoView(url: String) {
+        video path 
+
+        Button2
+
+        // parses the URL and converts it to Uri, which is needed for viewing a video
+        val video: Uri = Uri.parse(url) 
+
+        // This requests an app capable of viewing a video to do that job.  We may need to go back and specify a default web browser.
+        val intent = Intent(Intent.ACTION_VIEW, video)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            Log.v("MainActivity", "Intent resolved successfully.")
+            startActivity(chooser)
+        } else {
+            Log.d("MainActivity", "No application can handle this intent.")
+        }
+
+        video.start
+          log.v
     }
 }
