d7c7d5a26d4092b5c345aba7bd23e0d9bc56d893
Ronald Crabtree <rrc124@msstate.edu>
2024-11-14 21:14:55 -0600

    Added detailed documentation using CCE

--- ChildrensTherapy/app/src/main/java/com/example/childrenstherapy/MainActivity.kt
+++ ChildrensTherapy/app/src/main/java/com/example/childrenstherapy/MainActivity.kt
@@ -6,82 +6,118 @@ import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.net.Uri
+import android.widget.ImageView
 import android.widget.VideoView
 import android.widget.MediaController
-
-
-
 /*
-Syntax: class MainActivity "of" (:) AppCompatActivity()
-AppCompatActivity() supports modern themes while offering compatibility to older devices.
-This is set when you choose backwards compatibility upon setting up the project.
- */
+# Children's Therapy App and General Overview of Kotlin <br>
+ <br>
 
-/*
-Followed walkthrough from https://www.geeksforgeeks.org/videoview-in-android/ and used chatgpt
-for debugging and helping to close some loose ends.
- */
+# Children's Therapy App <br>
+The ***MainActivity*** class is the entry point of the app. The app uses *3x3* grid of buttons 
+to control actions such as playing videos or showing images.  This class interacts with *activity_main.xml*
+to set the layout on the user's screen and the behavior of the app. Each button in the 3x3 grid performs a 
+specific action such as opening a webpage or playing a video.<br><br>
+The Children's Therapy App currently includes three main functions: 
+1. **Opening a webpage** <br><br>
+Button 1 opens a web browser and begins playing a video linked in variable ***webpageUrl*** 
+
+2. **Play Video From Directory** <br><br>
+Button 2 accesses a video file from the internal file directory of an Android device. 
+
+3. **Show Image From Directory** <br><br>
+Button 3 accesses a gif image file from the internal file directory of an Android device. 
+This image cannot yet play the gif, but shows as a still image only.
+<br>
+<br>
+## Kotlin
+1. ***class MainActivity "of" (:) AppCompatActivity()***
+<br><br>
+Similar to int main() entry point in C++, class MainActivity() is the entry point of the program.
+<br>***AppCompatActivity()*** provides backwards compatibility for android devices. By setting ***MainActivity()*** 
+as a subclass of ***AppCompatActivity()***, we ensure that our main function inherits all of the backwards
+compatibility and adaptability (such as toolbars and themes) for older devices.
+
+2. ***private lateinit var videoView: VideoView***<br><br>
+*lateinit* is a kotlin keyword that alows us to declare variables that we will use later without having to<br>
+set them to null.<br>
+
+3. ***super.onCreate(savedInstanceState)***<br><br>
+When we override a function, we can use super to call the superclass' version of the method being c alled. 
+The superclass in this case is AppCompatActivity()<br>
+
+4. ***OnClickListener*** "listens" for button presses.<br><br>
+*findViewById<Button>(R.id.button_open_webpage).setOnClickListener* finds the button in *activity_main.xml* with the
+id: button_open_webpage, then completes the actions between {} after the button is pressed.  <br>
+
+5. ***CatLog for Debugging***<br><br>
+Use Log with tag messages to keep track of the program processes in real time as you run the program.  This aids
+in understanding if actions such as button presses were successful.<br>
+Log methods are: *Log.b* (debugging); *Log.v* (shows everything, including low level actions); *Log.i* (general info); 
+*Log.w* (warnings); *Log.e* (error messages)<br>
+
+6. ***Interact with the App's File System***<br><br>
+*android.resource*android.resource* is how we tell the program to look into the Android's file system for items 
+such as videos or images.  By using the *Uri.parse("android.resource://" + packageName + "/" + R.raw.monkey_video)*, 
+the app will find *monkey_video* in the *res/raw* folder, the path that provides access to resources within the app.<br><br> 
+*/
+
 class MainActivity : AppCompatActivity() {
     private lateinit var videoView: VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        // When we override a function, we can use super.method to call the superclass' version of the method
         super.onCreate(savedInstanceState)
 
-        // To see what this is doing, go to activity_main.xml
         setContentView(R.layout.activity_main)
 
+        // videoView was already declared with lateinit, so no var/val needed here
         videoView = findViewById(R.id.idVideoView)
 
         // Sets up click listener for the button
-        // button_open_webpage is an ID used in activity_main.xml, which is expected to be assigned to a <button>.
-        // Dot notation used to call the .setOnClickListener method.
         findViewById<Button>(R.id.button_open_webpage).setOnClickListener {
             val webpageUrl = "https://youtu.be/h2vxQ8Tewok?si=ZVWz5w_L_vrn1wuN"
             openWebPage(webpageUrl)
-            // Log.d sends debugging information to Logcat so we can view what's going on.
-            // Other methods in Log.<method> are: Log.v (low level info); Log.i (general info); Log.w (warnings); Log.e (error messages)
+            // Track actions while the app runs for debugging.
             Log.d("MainActivity", "Open Webpage button clicked")
         }
 
         findViewById<Button>(R.id.monkey_video).setOnClickListener {
-            // Log.d sends debugging information to Logcat so we can view what's going on.
-            // Other methods in Log.<method> are: Log.v (low level info); Log.i (general info); Log.w (warnings); Log.e (error messages)
+
             Log.v("MainActivity", "Monkey Button clicked")
 
+            // Provides video player controls such as pause and play.
             val mediaController = MediaController(this)
+            // Overlays video player controls onto videoView so the controls appear at the bottom of the video
             mediaController.setAnchorView(videoView)
 
             videoView.setMediaController(mediaController)
 
-            // "android.resource" is how we tell the program to look into the Android's file system.
-
             val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.monkey_video)
+            // Tells videoView to play the video assigned to val uri (monkey_video).
             videoView.setVideoURI(uri)
             videoView.start()
         }
+        findViewById<Button>(R.id.toad).setOnClickListener {
+            Log.v("MainActivity", "Toad Button clicked")
 
-    }
-
-    // Function takes a URL to op0en a webpage.
-    private fun openWebPage(url: String) {
-        // parses the URL and converts it to Uri, which is needed for viewing a webpage
-        val webpage: Uri = Uri.parse(url)
-
-        // The reason our program is not pulling up the webpage probably is linked to this line of code.
-        // This requests an app capable of viewing a webpage to do that job.  We may need to go back and specify a default web browser.
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-
-        // Add logging to check if the intent resolves successfully
-        // Throws a error that no webpage handling app is available
-        val chooser = Intent.createChooser(intent, "Select a browser")
-        if (chooser.resolveActivity(packageManager) != null) {
-            Log.v("MainActivity", "Intent resolved successfully.")
-            startActivity(chooser)
-        } else {
-            Log.d("MainActivity", "No application can handle this intent.")
+            val toadImage = findViewById<ImageView>(R.id.toadView)
+            toadImage.setImageResource(R.drawable.toad)
         }
     }
+}
+// Function takes a URL to op0en a webpage.
+private fun openWebPage(url: String) {
+    // parses the URL and converts it to Uri, which is needed for viewing a webpage.
+    val webpage: Uri = Uri.parse(url)
 
+    // Requests an app capable of viewing a webpage.
+    val intent = Intent(Intent.ACTION_VIEW, webpage)
+
+    val chooser = Intent.createChooser(intent, "Select a browser")
+    if (chooser.resolveActivity(packageManager) != null) {
+        Log.v("MainActivity", "Intent resolved successfully.")
+        startActivity(chooser)
+    } else {
+        Log.d("MainActivity", "No application can handle this intent.")
+    }
 }
